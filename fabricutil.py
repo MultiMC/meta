9207257bb96377b2f2dc7ec876348f78281f0ccd
Petr Mrazek <peterix@gmail.com>
2023-12-01 22:56:41 +0100

    Add support for new Fabric metadata format
    A bit limited, but it appears to be working

--- fabricutil.py
+++ fabricutil.py
@@ -9,17 +9,55 @@ class FabricInstallerArguments(JsonObject):
 class FabricInstallerLaunchwrapper(JsonObject):
     tweakers = ObjectProperty(FabricInstallerArguments, required=True)
 
-class FabricInstallerLibraries(JsonObject):
+class FabricInstallerLibrariesV1(JsonObject):
     client = ListProperty(MultiMCLibrary)
     common = ListProperty(MultiMCLibrary)
     server = ListProperty(MultiMCLibrary)
+    development = ListProperty(MultiMCLibrary, required=False)
 
 class FabricInstallerDataV1(JsonObject):
     version = IntegerProperty(required=True)
-    libraries = ObjectProperty(FabricInstallerLibraries, required=True)
+    libraries = ObjectProperty(FabricInstallerLibrariesV1, required=True)
     mainClass = jsonobject.DefaultProperty()
     arguments = ObjectProperty(FabricInstallerArguments, required=False)
     launchwrapper = ObjectProperty(FabricInstallerLaunchwrapper, required=False)
+
+'''
+
+This format was introduced with version 0.15.0
+{
+    "name": "org.ow2.asm:asm:9.6",
+    "md5": "6f8bccf756f170d4185bb24c8c2d2020",
+    "sha1": "aa205cf0a06dbd8e04ece91c0b37c3f5d567546a",
+    "sha256": "3c6fac2424db3d4a853b669f4e3d1d9c3c552235e19a319673f887083c2303a1",
+    "sha512": "01a5ea6f5b43bf094c52a50e18325a60af7bb02e74d24f9bc2c727d43e514578fd968b30ff22f9d2720caec071458f9ff82d11a21fbb1ebc42d8203e737c4b52",
+    "size": 123598,
+    "url": "https://maven.fabricmc.net/"
+},
+'''
+
+class FabricLibrary (JsonObject):
+    name = GradleSpecifierProperty(required = True)
+    url = StringProperty(exclude_if_none=True, default=None)
+    size = IntegerProperty()
+    md5 = StringProperty(exclude_if_none=True, default=None)
+    sha1 = StringProperty(exclude_if_none=True, default=None)
+    sha256 = StringProperty(exclude_if_none=True, default=None)
+    sha512 = StringProperty(exclude_if_none=True, default=None)
+
+
+class FabricInstallerLibrariesV2(JsonObject):
+    client = ListProperty(FabricLibrary)
+    common = ListProperty(FabricLibrary)
+    server = ListProperty(FabricLibrary)
+    development = ListProperty(FabricLibrary, required=False)
+
+class FabricInstallerDataV2(JsonObject):
+    version = IntegerProperty(required=True)
+    libraries = ObjectProperty(FabricInstallerLibrariesV2, required=True)
+    mainClass = jsonobject.DefaultProperty()
+    min_java_version = IntegerProperty(required=True)
+
 
 class FabricJarInfo(JsonObject):
     releaseTime = ISOTimestampProperty()
--- generateFabric.py
+++ generateFabric.py
@@ -22,7 +22,12 @@ def loadJarInfo(mavenKey):
     with open("upstream/fabric/jars/" + mavenKey.replace(":", ".") + ".json", 'r', encoding='utf-8') as jarInfoFile:
         return FabricJarInfo(json.load(jarInfoFile))
 
-def processLoaderVersion(loaderVersion, it, loaderData):
+def toMultiMCLibrary(fabricLibrary):
+    'TODO: maybe we can actually use the sha1 or something...'
+    return MultiMCLibrary(name=fabricLibrary.name, url=fabricLibrary.url)
+
+def processLoaderVersionV1(loaderVersion, it, loaderData):
+    loaderData = FabricInstallerDataV1(loaderData)
     verStable = it["stable"]
     if (len(loaderRecommended) < 1) and verStable:
         loaderRecommended.append(loaderVersion)
@@ -39,6 +44,29 @@ def processLoaderVersion(loaderVersion, it, loaderData):
     version.libraries = []
     version.libraries.extend(loaderData.libraries.common)
     version.libraries.extend(loaderData.libraries.client)
+    loaderLib = MultiMCLibrary(name=GradleSpecifier(it["maven"]), url="https://maven.fabricmc.net")
+    version.libraries.append(loaderLib)
+    loaderVersions.append(version)
+
+def processLoaderVersionV2(loaderVersion, it, loaderData):
+    'TODO: use min_java_version to fill in the equivalent of it in the final file(s) bonce we have that in place'
+    loaderData = FabricInstallerDataV2(loaderData)
+    verStable = it["stable"]
+    if (len(loaderRecommended) < 1) and verStable:
+        loaderRecommended.append(loaderVersion)
+    versionJarInfo = loadJarInfo(it["maven"])
+    version = MultiMCVersionFile(name="Fabric Loader", uid="net.fabricmc.fabric-loader", version=loaderVersion)
+    version.releaseTime = versionJarInfo.releaseTime
+    version.requires = [DependencyEntry(uid='net.fabricmc.intermediary')]
+    version.order = 10
+    version.type = "release"
+    if isinstance(loaderData.mainClass, dict):
+        version.mainClass = loaderData.mainClass["client"]
+    else:
+        version.mainClass = loaderData.mainClass
+    version.libraries = []
+    version.libraries.extend(list(map(toMultiMCLibrary, loaderData.libraries.common)))
+    version.libraries.extend(list(map(toMultiMCLibrary, loaderData.libraries.common)))
     loaderLib = MultiMCLibrary(name=GradleSpecifier(it["maven"]), url="https://maven.fabricmc.net")
     version.libraries.append(loaderLib)
     loaderVersions.append(version)
@@ -63,8 +91,13 @@ with open("upstream/fabric/meta-v2/loader.json", 'r', encoding='utf-8') as loade
         version = it["version"]
         with open("upstream/fabric/loader-installer-json/" + version + ".json", 'r', encoding='utf-8') as loaderVersionFile:
             ldata = json.load(loaderVersionFile)
-            ldata = FabricInstallerDataV1(ldata)
-            processLoaderVersion(version, it, ldata)
+            loaderVersion = ldata['version']
+            if loaderVersion == 1:
+                processLoaderVersionV1(version, it, ldata)
+            elif loaderVersion == 2:
+                processLoaderVersionV2(version, it, ldata)
+            else:
+                raise UnknownVersionException("Unsupported Fabric format version: %d. Max supported is: 2" % (loaderVersion))
 
 with open("upstream/fabric/meta-v2/intermediary.json", 'r', encoding='utf-8') as intermediaryVersionIndexFile:
     intermediaryVersionIndex = json.load(intermediaryVersionIndexFile)
